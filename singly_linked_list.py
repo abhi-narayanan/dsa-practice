554747864f76fb9dff2b8340ee95c7c8ff535b64
Narayanan <abhishek.narayanan@accenture.com>
2025-12-17 14:48:55 +0530

    added more functions to linked list class

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -308,22 +308,114 @@ class LinkedList:
         self.head = p.next
         p.next = None
 
+    # RACECAR, RADAR, TEST, ABC, HELLO
+    def is_palindrome(self):
+        # # Method 1
+        # s = ""
+        # p = self.head
+
+        # while p:
+        #     s += p.data
+        #     p = p.next
+
+        # return s == s[::-1]
+
+        # # Method 2
+        # p = self.head
+        # s = []
+        # while p:
+        #     s.append(p.data)
+        #     p = p.next
+        # p = self.head
+        # while p:
+        #     data = s.pop()
+        #     if p.data != data:
+        #         return False
+        #     p = p.next
+        # return True
+
+        # Method 3
+        p = self.head
+        q = self.head
+        prev = []
+
+        i = 0
+        while q:
+            prev.append(q)
+            q = q.next
+            i += 1
+        q = prev[i-1]
+
+        count = 1
+        while count <= i//2 + 1:
+            if prev[-count].data != p.data:
+                return False
+            p = p.next
+            count += 1
+        return True
+    
+    def move_tail_to_head(self):
+        """
+        A -> B -> C -> D
+        D -> B -> C -> A
+        """
+        last = self.head
+        second_to_last = None
+
+        while last.next:
+            second_to_last = last
+            last = last.next
+        last.next = self.head
+        second_to_last.next = None
+        self.head = last
+    
+    def sum_two_lists(self, llist):
+        """
+        3 6 5
+        2 4 8
+        (+)
+        6 1 3
+        """
+        p = self.head
+        q = llist.head
+        sum_llist = LinkedList()
+        carry = 0
+        
+        while p or q:
+            if not p:
+                i = 0
+            else:
+                i = p.data
+            if not q:
+                j = 0
+            else:
+                j = q.data
+            s = i + j + carry
+            if s >= 10:
+                carry = 1
+                remainder = s % 10
+                sum_llist.append(remainder)
+            else:
+                carry = 0
+                sum_llist.append(s)
+            if p:
+                p = p.next
+            if q:
+                q = q.next
+        sum_llist.print_list()
+            
+
 llist_1 = LinkedList()
-llist_1.append(1)
-llist_1.append(2)
-llist_1.append(3)
-llist_1.append(4)
 llist_1.append(5)
 llist_1.append(6)
+llist_1.append(3)
 
 llist_2 = LinkedList()
-llist_2.append("2")
-llist_2.append("3")
-llist_2.append("4")
-llist_2.append("6")
-llist_2.append("8")
+llist_2.append(8)
+llist_2.append(4)
+llist_2.append(2)
 
-llist_1.rotate(4)
+# llist_1.rotate(4)
 
 # llist_1.remove_duplicates()
 
@@ -342,7 +434,13 @@ llist_1.rotate(4)
 
 # print(llist.len_recursive(llist.head.next))
 
-llist_1.print_list()
+# print(llist_2.is_palindrome())
+
+# llist_1.move_tail_to_head()
+
+llist_1.sum_two_lists(llist_2)
+
+# llist_1.print_list()
 
 """
 Given a key (data field) delete node with this field. Assume elements in linked list are unique. Example: Delete node with data field "B".
