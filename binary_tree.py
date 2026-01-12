ad63edd2762f29055a9f2d847da533edd2b70d18
abhi-narayanan <myselfabhishekn@gmail.com>
2026-01-12 15:25:08 +0530

    tree height function added

--- binary_tree.py
+++ binary_tree.py
@@ -115,6 +115,15 @@ class BinaryTree(object):
             tranversal += str(node.value) + "-"
         return tranversal
     
+    def height(self, node):
+        if node is None:
+            return -1
+        
+        left_height = self.height(node.left)
+        right_height = self.height(node.right)
+
+        return 1 + max(left_height, right_height)
+        
 tree = BinaryTree(1)
 tree.root.left = Node(2)
 tree.root.right = Node(3)
@@ -128,4 +137,6 @@ tree.root.right.right.right = Node(8)
 # print(tree.print_tree("inorder"))
 # print(tree.print_tree("postorder"))
 # print(tree.print_tree("levelorder"))
-print(tree.print_tree("reverse_levelorder"))
+# print(tree.print_tree("reverse_levelorder"))
+
+print(tree.height(tree.root))
